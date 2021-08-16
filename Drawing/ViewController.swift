589d9602690808d05895a9c075323022529d1937
ShaanPurewal <Purewal002@gmail.com>
2021-08-16 16:28:20 -0400

    Initial Commit, Basic drawing applications implemented/PencilKit Support

--- Drawing/ViewController.swift
+++ Drawing/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import PencilKit
+import PhotosUI
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
+    
+    @IBOutlet weak var canvasView: PKCanvasView!
+    
+    var drawing = PKDrawing()
+    var toolPicker: PKToolPicker!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        canvasView.delegate = self
+        canvasView.drawing = drawing
+        
+        canvasView.alwaysBounceVertical = true
+        canvasView.drawingPolicy = PKCanvasViewDrawingPolicy.anyInput
+        
+        toolPicker = PKToolPicker()
+        
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        toolPicker.addObserver(canvasView)
+        canvasView.becomeFirstResponder()
+                
     }
-
-
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    @IBAction func saveDrawingToCameraRoll(_ sender: Any) {
+        UIGraphicsBeginImageContextWithOptions(canvasView.bounds.size, false, UIScreen.main.scale)
+        
+        canvasView.drawHierarchy(in: canvasView.bounds, afterScreenUpdates: true)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        if image != nil {
+            PHPhotoLibrary.shared().performChanges({PHAssetChangeRequest.creationRequestForAsset(from: image!)}, completionHandler: {success, error in
+            })
+            
+            let alert = UIAlertController(title: "Note Saved", message: "Your note has been saved to your photo library", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+        }else {
+            let alert = UIAlertController(title: "Note Not Saved", message: "Your note has encountered and error while saving to your photo library", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+        }
+        
+    }
+    
+    @IBAction func clearCanvas(_ sender: Any) {
+        canvasView.drawing = PKDrawing()
+    }
+    
 }
 
